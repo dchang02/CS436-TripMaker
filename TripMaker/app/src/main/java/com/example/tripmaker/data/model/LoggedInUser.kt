009a57dcf633c31847bd0bfb8d586d228b0b2c81
laurenmanning <lemanning3@gmail.com>
2022-11-11 16:13:03 -0500

    fixed login page

---
+++ TripMaker/app/src/main/java/com/example/tripmaker/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.tripmaker.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
